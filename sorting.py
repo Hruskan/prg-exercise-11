752ba70e6bd1d20c3dd94da807a3a8a4108a5429
Hruskan <mostmatousek@gmail.com>
2026-04-22 09:22:04 +0200

    bubble_sort

--- sorting.py
+++ sorting.py
@@ -13,12 +13,20 @@ def selection_sort(numbers):
                 min_index = i
                 min_value = values[i]
         values[u], values[min_index] = values[min_index], values[u]
-        print(values)
+    return values
+
+def bubble_sort(numbers):
+    values = numbers.copy()
+    for u in range(len(values)-1):
+        for i in range(len(values)-u-1):
+            if values[i] > values[i+1]:
+                values[i], values[i+1] = values[i+1], values[i]
     return values
 
 if __name__ == "__main__":
     values = random_numbers(10)  # 10 čísel v rozsahu 0–100
     print(values)  # např. [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
-    selection_sort(values)
+    print(selection_sort(values))
+    print(bubble_sort(values))
     small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
 
